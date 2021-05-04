98526981483ae98bec59a38794fedf5dea7cbfab
DinaShafeeva <di.shafeeva.r@gmail.com>
2021-05-04 22:08:03 +0300

    Changed mapkit to mapbox

--- app/src/main/java/com/example/miniguide/MainActivity.kt
+++ app/src/main/java/com/example/miniguide/MainActivity.kt
@@ -1,18 +1,11 @@
 package com.example.miniguide
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.miniguide.ui.MapActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        button.setOnClickListener{
-            val intent = Intent(this, MapActivity::class.java)
-            startActivity(intent)
-        }
     }
 }
---
+++ app/src/main/java/com/example/miniguide/ui/MapFragment.kt
@@ -0,0 +1,145 @@
+package com.example.miniguide.ui
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.graphics.Color
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import com.example.miniguide.R
+import com.example.miniguide.helper.askPermissionsSafely
+import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.location.*
+import com.mapbox.mapboxsdk.location.modes.CameraMode
+import com.mapbox.mapboxsdk.location.modes.RenderMode
+import com.mapbox.mapboxsdk.maps.MapboxMap
+import com.mapbox.mapboxsdk.maps.Style
+import kotlinx.android.synthetic.main.fragment_map.*
+import kotlinx.coroutines.launch
+
+
+class MapFragment : Fragment() {
+
+    private val onLocationClickListener = OnLocationClickListener {
+        Log.e("Callback - ", " OnLocationClickListener")
+    }
+
+    private val onCameraTrackingChangedListener = object : OnCameraTrackingChangedListener {
+        override fun onCameraTrackingDismissed() {
+            Log.e("Callback - ", " onCameraTrackingChangedListener")
+
+        }
+
+        override fun onCameraTrackingChanged(currentMode: Int) {
+            Log.e("Callback - ", " onCameraTrackingChanged")
+        }
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        Mapbox.getInstance(requireContext(), getString(R.string.mapbox_access_token))
+        return inflater.inflate(R.layout.fragment_map, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        requestMapPermissions()
+        mapView?.onCreate(savedInstanceState)
+    }
+
+    private fun initMapView() {
+        mapView?.getMapAsync { mapboxMap ->
+            mapboxMap.setStyle(Style.MAPBOX_STREETS) {
+                initLocationComponent(mapboxMap, it)
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun initLocationComponent(mapboxMap: MapboxMap, style: Style) {
+        val customLocationComponentOptions = LocationComponentOptions.builder(requireContext())
+            .elevation(5f)
+            .accuracyAlpha(.6f)
+            .accuracyColor(Color.RED)
+            .foregroundDrawable(R.drawable.ic_launcher_foreground)
+            .build()
+
+        mapboxMap.apply {
+
+            val locationComponentActivationOptions =
+                LocationComponentActivationOptions.builder(requireContext(), style)
+                    .locationComponentOptions(customLocationComponentOptions)
+                    .build()
+
+            // Activate with options
+            locationComponent.activateLocationComponent(locationComponentActivationOptions)
+
+            // Enable to make component visible
+            locationComponent.isLocationComponentEnabled = true
+
+            // Set the component's camera mode
+            locationComponent.cameraMode = CameraMode.TRACKING
+
+            // Set the component's render mode
+            locationComponent.renderMode = RenderMode.COMPASS
+
+            // Add the location icon click listener
+            locationComponent.addOnLocationClickListener(onLocationClickListener)
+
+            // Add the camera tracking listener. Fires if the map camera is manually moved.
+            locationComponent.addOnCameraTrackingChangedListener(onCameraTrackingChangedListener)
+
+        }
+    }
+
+    private fun requestMapPermissions() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            val result = askPermissionsSafely(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+
+            if (result.isSuccess) {
+                initMapView()
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView?.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView?.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView?.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView?.onStop()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView?.onLowMemory()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView?.onDestroy()
+    }
+}
